4bcce55c5fb96c4957c00115c12af5f44eedea66
Gustavo Rivera <garp2100@gmail.com>
2022-08-01 22:02:52 -0400

    adding Car and UsedCar classes along with constructor inheriting from base class like suggested

---
+++ UsedCar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsedCarLotLab
+{
+    public class UsedCar : Car
+    {
+        //Constructor
+        public UsedCar(string _make, string _model, int _year, decimal _price, double _mileage) : base(_make, _model, _year, _price) 
+        { 
+            Mileage = _mileage; 
+        }      
+        public double Mileage { get; set; }
+    }     
+}
