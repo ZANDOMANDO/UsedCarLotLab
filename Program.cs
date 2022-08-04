f209756c2c7b4c1acebe4f1d2a965a33f3574de4
Gustavo Rivera <garp2100@gmail.com>
2022-08-03 21:51:05 -0400

    adding CarLot class

--- Program.cs
+++ Program.cs
@@ -2,7 +2,9 @@
 var cruze = new  UsedCar("Chevrolet", "Cruze", 2015, 10000.00m, 63000.00);
 var rav4 = new UsedCar("Toyota", "Rav4", 2016, 21000.00m, 62000.00);
 var flex = new UsedCar("Ford", "Flex", 2019, 20000.00m, 48000.00);
+
 var aventador = new Car("Lambourgini", "Aventador", 2022, 300000.00m);
 var modelSPlaid = new Car("Tesla", "Model S Plaid", 2022, 200000.00m);
 var ferrari = new Car("Ferrari", "F8 Spider", 2022, 302500.00m);
 
+//methods
